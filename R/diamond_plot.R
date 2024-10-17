b612ed71ceb892b6f18d3ae3356a6e801353942c
Jianuo Liu <ljnyklqzx@163.com>
2024-10-17 19:54:29 -0400

    Writing a R package

---
+++ R/diamond_plot.R
@@ -0,0 +1,24 @@
+#' Diamond Plot
+#'
+#' This function produces a ggplot of \code{vary} versus \code{varx} with diamond markers.
+#'
+#' @param df  a dataframe
+#' @param varx  a string name for variable x in the dataframe df
+#' @param vary  a string name for variable y in the dataframe df
+#'
+#' @return A plot with diamond markers of \code{vary} versus \code{varx}
+#' @export
+#'
+#' @examples
+diamond_plot <- function(df, varx, vary) {
+  ggplot(df, aes(x=get(varx), y=get(vary))) +
+    geom_point(shape=23, fill="blue", color="darkred", size=3)
+}
+
+library(ggplot2)
+
+df.test <- data.frame(x=rnorm(10),
+                      y=rnorm(10))
+
+diamond_plot(df.test, 'x', 'y')
+
